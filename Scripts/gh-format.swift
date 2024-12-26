99dd43c5933f43afc70f94a503d70a718d729a9a
Alexander Ignition <izh.sever@gmail.com>
2024-12-26 10:37:43 +0300

    gh-format

--- Scripts/gh-format.swift
+++ Scripts/gh-format.swift
@@ -2,7 +2,7 @@
 
 // Workflow commands for GitHub Actions
 // https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions
-// Usage: xcrun swift-format lint --recursive --strict ./ 2>&1 | Scripts/gh-workflow.swift
+// Usage: xcrun swift-format lint --recursive --strict ./ 2>&1 | Scripts/gh-format.swift
 // Test: echo "Sources/SQLime/SQLParameter.swift:23:1: warning: [TrailingWhitespace] remove trailing whitespace" | Scripts/gh-workflow.swift
 
 let regex = #/(?<file>.+\.swift):(?<line>\d+):(?<column>\d+): (?<severity>.+): \[(?<title>.+)\] (?<message>.+)/#
