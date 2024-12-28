8b40775e9abeb314c60f4b8ad40ffc9a8cf12f7f
Alexander Ignition <izh.sever@gmail.com>
2024-12-28 16:01:13 +0300

    Update DatabaseError.swift

--- Sources/SQLyra/DatabaseError.swift
+++ Sources/SQLyra/DatabaseError.swift
@@ -1,3 +1,4 @@
+import Foundation
 import SQLite3
 
 /// SQLite database error.
@@ -6,10 +7,10 @@ public struct DatabaseError: Error, Equatable, Hashable {
     public let code: Int32
 
     /// A short error description.
-    public var message: String
+    public var message: String?
 
     /// A complete sentence (or more) describing why the operation failed.
-    public let reason: String
+    public let reason: String?
 
     /// A new database error.
     ///
@@ -25,19 +26,45 @@ public struct DatabaseError: Error, Equatable, Hashable {
 
     init(code: Int32, db: OpaquePointer?) {
         self.code = code
-        self.message = sqlite3_errstr(code).string ?? ""
-        self.reason = sqlite3_errmsg(db).string ?? ""
+        self.message = sqlite3_errstr(code).string
+        self.reason = sqlite3_errmsg(db).string
     }
 }
+
+// MARK: - LocalizedError
+
+extension DatabaseError: LocalizedError {
+    public var errorDescription: String? { message }
+    public var failureReason: String? { reason }
+}
+
+// MARK: - CustomNSError
+
+extension DatabaseError: CustomNSError {
+    public static let errorDomain = "SQLyra.DatabaseError"
+
+    public var errorCode: Int { Int(code) }
+
+    public var errorUserInfo: [String: Any] {
+        var userInfo: [String: Any] = [:]
+        userInfo[NSLocalizedDescriptionKey] = errorDescription
+        userInfo[NSLocalizedFailureReasonErrorKey] = failureReason
+        return userInfo
+    }
+}
+
+// MARK: - DatabaseHandle
 
 protocol DatabaseHandle {
     var db: OpaquePointer! { get }
 }
 
 extension DatabaseHandle {
-    func check(_ code: Int32, _ success: Int32 = SQLITE_OK) throws {
-        if code != success {
+    @discardableResult
+    func check(_ code: Int32, _ success: Int32 = SQLITE_OK) throws -> Self {
+        guard code == success else {
             throw DatabaseError(code: code, db: db)
         }
+        return self
     }
 }
